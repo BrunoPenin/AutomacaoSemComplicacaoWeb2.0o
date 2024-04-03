0841b6d6b1a222f54025c271c2c4fb2bd3de4def
Bruno Penin <you@example.com>
2024-04-03 20:01:09 -0300

    first commit

---
+++ src/test/java/br/com/cromosAcademy/media/MediaTest.java
@@ -0,0 +1,26 @@
+package br.com.cromosAcademy.media;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MediaTest {
+    //Aprovado
+    //Reprovado
+
+    @Test
+    public void validaAprovado(){
+        Media media = new Media();
+        String resultado = media.calculaMedia(5.0,5.0);
+        assertEquals("Aprovado", resultado);
+
+    }
+
+    @Test
+    public void validaReprovado(){
+        Media xuxa = new Media();
+        String resultado = xuxa.calculaMedia(4.9,5.0);
+        assertEquals("Reprovado", resultado);
+
+    }
+}
