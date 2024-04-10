136f882f5f3740b0ab49d40781e7a550dce4e82a
Bruno Penin <you@example.com>
2024-04-10 14:16:34 -0300

    aula13 e 14, cucumber e runner

---
+++ src/test/java/br/com/chronosacademy/steps/LoginSteps.java
@@ -0,0 +1,68 @@
+package br.com.chronosacademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+    @Dado("que a modal estea sendo exibida")
+    public void queAModalEsteaSendoExibida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("for realiazdo um clique fora da modal")
+    public void forRealiazdoUmCliqueForaDaModal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("a janela modal deve ser fechada")
+    public void aJanelaModalDeveSerFechada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("for realizado um clique no icone de fechar modal")
+    public void forRealizadoUmCliqueNoIconeDeFecharModal() {
+        
+    }
+
+    @Quando("for relizado um clique no link Create New Account")
+    public void forRelizadoUmCliqueNoLinkCreateNewAccount() {
+        
+    }
+
+    @Então("a pagina Create Ne Account deve ser exibida")
+    public void aPaginaCreateNeAccountDeveSerExibida() {
+        
+    }
+
+    @Quando("os campos de login sejam preenchidos da seguinte forma")
+    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma() {
+        
+    }
+
+    @Quando("for realizado o clique no botao sign in")
+    public void forRealizadoOCliqueNoBotaoSignIn() {
+        
+    }
+
+    @Então("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+        
+    }
+
+    @Quando("for realizado o clique no botão sign in")
+    public void forRealizadoOCliqueNoBotãoSignIn() {
+        
+    }
+
+    @Então("o sistema devera exibir uma mensagem de erro")
+    public void oSistemaDeveraExibirUmaMensagemDeErro() {
+        
+    }
+
+    @Entao("o botao sign in deve permanecer desabilitado")
+    public void oBotaoSignInDevePermanecerDesabilitado() {
+    }
+}
