c019323c9c5f4f9e77f02218e7bb967909d97f74
Bruno Penin <you@example.com>
2024-04-10 17:09:18 -0300

    aula15 Cucumber Maps e Pages

---
+++ src/test/java/br/com/chronosacademy/LoginMap.java
@@ -0,0 +1,16 @@
+package br.com.chronosacademy;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFechaModal;
+    public WebElement inputUserName;
+    public WebElement inpPassword;
+    public WebElement btnSignIn;
+    public WebElement inpRemember;
+    public WebElement linkCreteAccount;
+
+}
